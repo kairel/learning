9e8b2d9b6a9fbfc936c48f6e8287f4c486f9c854
malveslobo <malveslobo@click2buy.com>
2022-12-15 20:52:59 +0100

    Added terraform alertmanager

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# declare provider
 terraform {
   required_providers {
     docker = {
@@ -11,6 +12,11 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+resource "docker_network" "prometheus" {
+  name   = "prometheus"
+  driver = "bridge"
+}
+
 resource "docker_container" "prometheus" {
   name  = "prometheus"
   image = "prom/prometheus"
@@ -19,4 +25,39 @@ resource "docker_container" "prometheus" {
     internal = "9090"
     external = "9090"
   }
+
+  volumes {
+    container_path  = "/etc/prometheus/prometheus.yml"
+    read_only = false
+    host_path = "/tmp/prometheus/prometheus.yml"
+  }
+
+  networks_advanced {
+    name    = docker_network.prometheus.name
+    aliases = ["prometheus"]
+  }
+
 }
+
+resource "docker_container" "alertmanager" {
+  name  = "alertmanager"
+  image = "prom/alertmanager"
+
+  ports {
+    internal = "9093"
+    external = "9093"
+  }
+
+  volumes {
+    container_path  = "/etc/alertmanager/conf/alertmanager.conf"
+    read_only = false
+    host_path = "/tmp/prometheus/alertmanager.conf"
+  }
+
+  networks_advanced {
+    name    = docker_network.prometheus.name
+    aliases = ["prometheus"]
+  }
+
+}
+
